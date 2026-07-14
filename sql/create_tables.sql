153a5d39558671acf88f6bc275796393411b75a4
Sarthak Bisht <bsarthak088@gmail.com>
2026-07-14 18:04:18 +0530

    Update create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,24 +1,26 @@
-CREATE TABLE customers(
+create table customers(
 customer_id VARCHAR(50),
 customer_unique_id VARCHAR(50),
 customer_zip_code_prefix INT,
 customer_city VARCHAR(100),
 customer_state CHAR(2)
 );
-CREATE TABLE orders(
+create table orders(
 order_id VARCHAR(50),
 customer_id VARCHAR(50),
 order_status VARCHAR(30),
 order_purchase_timestamp DATETIME
 );
-CREATE TABLE payments(
+
+create table payments(
 order_id VARCHAR(50),
 payment_sequential INT,
 payment_type VARCHAR(30),
 payment_installments INT,
 payment_value FLOAT
 );
-CREATE TABLE products(
+
+create table products(
 product_id VARCHAR(50),
 product_category_name VARCHAR(100)
 );
