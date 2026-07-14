c84d5fd44174c47af4cbc403f47462fcdd010cec
Sarthak Bisht <bsarthak088@gmail.com>
2026-07-14 17:55:02 +0530

    Update gold.py

--- scripts/gold.py
+++ scripts/gold.py
@@ -2,35 +2,32 @@ import os
 import pandas as pd
 from utils import create_folder
 
-SILVER = "data/silver"
-GOLD = "data/gold"
+SILVER="data/silver"
+GOLD="data/gold"
 
 create_folder(GOLD)
 
-print("=" * 60)
+print("==========")
 print("Creating Gold Layer")
-print("=" * 60)
+print("==========")
 
 customers = pd.read_parquet("data/silver/olist_customers_dataset.parquet")
 orders = pd.read_parquet("data/silver/olist_orders_dataset.parquet")
 payments = pd.read_parquet("data/silver/olist_order_payments_dataset.parquet")
-
 print("Customers :", customers.shape)
 print("Orders :", orders.shape)
 print("Payments :", payments.shape)
 
-# Remove duplicate keys
-customers = customers.drop_duplicates(subset="customer_id")
+customers=customers.drop_duplicates(subset="customer_id")
+orders=orders.drop_duplicates(subset="order_id")
 
-orders = orders.drop_duplicates(subset="order_id")
-
-payments = (
+payments=(
     payments
     .groupby("order_id", as_index=False)["payment_value"]
     .sum()
 )
 
-sales = pd.merge(
+sales=pd.merge(
     orders,
     customers,
     on="customer_id",
@@ -38,7 +35,7 @@ sales = pd.merge(
     validate="one_to_one"
 )
 
-sales = pd.merge(
+sales=pd.merge(
     sales,
     payments,
     on="order_id",
@@ -52,8 +49,7 @@ sales.to_parquet(
 )
 
 print("customer_sales.parquet created")
-
-state_sales = (
+state_sales=(
     sales.groupby("customer_state", as_index=False)
     .agg(
         Total_Orders=("order_id", "count"),
@@ -66,22 +62,18 @@ state_sales.to_parquet(
     "data/gold/state_sales.parquet",
     index=False
 )
-
 print("state_sales.parquet created")
-
-payment_summary = (
+payment_summary=(
     sales.groupby("order_status", as_index=False)
     .agg(
         Orders=("order_id", "count"),
         Revenue=("payment_value", "sum")
     )
 )
-
 payment_summary.to_parquet(
     "data/gold/order_summary.parquet",
     index=False
 )
 
 print("order_summary.parquet created")
-
 print("\nGold Layer Created Successfully")
