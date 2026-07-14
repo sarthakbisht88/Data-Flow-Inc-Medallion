2e4e5e5ecfc8e82b32b69569428106874f0e8248
Sarthak Bisht <bsarthak088@gmail.com>
2026-07-14 17:55:37 +0530

    Update scd_type2.py

--- scripts/scd_type2.py
+++ scripts/scd_type2.py
@@ -9,10 +9,9 @@ os.makedirs(OUTPUT, exist_ok=True)
 customers=pd.read_parquet(
     os.path.join(SILVER, "olist_customers_dataset.parquet")
 )
-
-customers["effective_date"] = datetime.today().strftime("%Y-%m-%d")
-customers["expiry_date"] = None
-customers["is_current"] = True
+customers["effective_date"]=datetime.today().strftime("%Y-%m-%d")
+customers["expiry_date"]=None
+customers["is_current"]=True
 customers.to_csv(
     os.path.join(OUTPUT, "customer_history.csv"),
     index=False
